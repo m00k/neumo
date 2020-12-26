9c8c4d30f37e835b7f4c8eb81dc0fb9d00238e22
m00k <christian.bican@gmail.com>
2020-12-26 18:54:33 +0100

    style(rounded buttons): add

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -2,6 +2,7 @@ import { FunctionalComponent, h } from "preact";
 import Countdown from "../../components/countdown";
 import * as style from "./style.css";
 import * as cardStyle from "../../components/card/style.css";
+import * as btnRoundStyle from "../../components/button/round.css";
 
 
 const Home: FunctionalComponent = () => {
@@ -10,8 +11,19 @@ const Home: FunctionalComponent = () => {
             <h1>Home</h1>
             <p>This is the Home component.</p>
             <Countdown />
-            <div class={`${cardStyle.card} ${style.card}`}>
-                Foo
+            <div class={`${cardStyle.card} ${style.card}`}>Foo</div>
+            <div class={style.action}>
+                <div
+                    class={`${style.btnRd} ${btnRoundStyle.btn} ${style.btnRdSmall}`}
+                ></div>
+                <div class={`${style.btnRd} ${btnRoundStyle.btn}`}></div>
+                <div
+                    class={`${style.btnRd} ${btnRoundStyle.btn} ${style.btnRdLarge}`}
+                ></div>
+                <div class={`${style.btnRd} ${btnRoundStyle.btn}`}></div>
+                <div
+                    class={`${style.btnRd} ${btnRoundStyle.btn} ${style.btnRdSmall}`}
+                ></div>
             </div>
         </div>
     );
