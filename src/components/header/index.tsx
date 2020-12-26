75dfeba7beaba8a9d5e771551b0c226a0bdb8e7e
m00k <christian.bican@gmail.com>
2020-12-26 17:28:56 +0100

    style(button): extract styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,25 @@
 import { FunctionalComponent, h } from "preact";
 import { Link } from "preact-router/match";
 import * as style from "./style.css";
+import * as btnStyle from "../button/style.css";
 
 const Header: FunctionalComponent = () => {
     return (
         <header class={style.header}>
             <h1>Neumo</h1>
             <nav>
-                <Link activeClassName={style.active} href="/">
+                <Link
+                    activeClassName={btnStyle.active}
+                    class={btnStyle.btn}
+                    href="/"
+                >
                     Home
                 </Link>
-                <Link activeClassName={style.active} href="/foo">
+                <Link
+                    activeClassName={btnStyle.active}
+                    class={btnStyle.btn}
+                    href="/foo"
+                >
                     Foo
                 </Link>
             </nav>
